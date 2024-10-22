d2e479aca4ae20eed8f4d998b51a4bfde55a1489
Monu Kumar <monu.deltatech@gmail.com>
2024-10-22 16:42:33 +0545

    first commit

---
+++ resources/views/front/layout/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>DevNex</title>
+    <link rel="icon" type="image/x-icon" href="{{asset('assets/image/logo.png')}}">
+
+    <!-- Vite CSS -->
+    @vite('resources/css/app.css')
+
+
+</head>
+
+
+<body>
+  @include('front.layout.navbar')
+df
+  <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
+
+</body>
+
+</html>
